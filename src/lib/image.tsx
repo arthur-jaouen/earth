ed457c5399501331deb064a38df111016ebc612c
Arthur JAOUEN <arthur.jaouen@gmail.com>
2023-08-16 13:17:04 +0200

    Add updated images

--- src/lib/image.tsx
+++ src/lib/image.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from 'react';
+import dayjs from 'dayjs';
+import { FunctionComponent, useEffect, useState } from 'react';
 
 import './image.scss';
 
@@ -9,15 +10,58 @@ export type ImageProps = {
   height?: number;
 };
 
-export const Image: FunctionComponent<ImageProps> = ({ url, alt, width, height }) => {
-  return (
-    <img
-      className="image"
-      src={url}
-      alt={alt}
-      title={`${url} - ${alt}`}
-      width={width}
-      height={height}
-    />
+export const Image: FunctionComponent<ImageProps> = ({ url, alt, width, height }) => (
+  <img
+    className="image"
+    src={url}
+    alt={alt}
+    title={`${url} - ${alt}`}
+    width={width}
+    height={height}
+  />
+);
+
+export async function getLatestImage(templateUrl: string): Promise<string | null> {
+  let date = dayjs();
+
+  for (let count = 0; count < 10; count++) {
+    try {
+      const url = date.format(templateUrl);
+      const response = await fetch(url, { method: 'HEAD' });
+
+      if (response.ok) {
+        return url;
+      }
+    } catch (error) {
+      // Nothing, just try next date
+    }
+
+    date = date.subtract(1, 'day');
+  }
+
+  return null;
+}
+
+export const TimeDependentImage: FunctionComponent<ImageProps> = ({ url, alt, width, height }) => {
+  const [latest, setLatest] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getLatestImage(url)
+      .then((latest) => {
+        setLatest(latest);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  return loading ? (
+    <div className="loader" />
+  ) : latest ? (
+    <Image url={latest} alt={alt} width={width} height={height} />
+  ) : (
+    <div className="placeholder">No recent image found</div>
   );
 };
